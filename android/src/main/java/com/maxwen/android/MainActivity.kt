244b935c8ee34297363333586f49fa33d63ff4e8
maxwen <max.weninger@gmail.com>
2023-10-23 00:41:30 +0200

    gerrit test 2
    Change-Id: I1be0058d0815e7e446b3f7e85f69067eabb8d33f

--- android/src/main/java/com/maxwen/android/MainActivity.kt
+++ android/src/main/java/com/maxwen/android/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // foo
+            // foo- bar
             MaterialTheme {
                 App()
             }
